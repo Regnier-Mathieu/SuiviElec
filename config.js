773690b7f210448986abd85ee5551ca56f8a5fc9
Mathieu <Mathieu@REGNIER.local>
2016-12-11 06:14:40 +0100

    Dependencies creation

---
+++ config.js
@@ -0,0 +1,10 @@
+let config = {
+
+	//lissening port
+	port: 4001
+
+	//database config
+	
+}
+
+module.exports = config
--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
 // Dependencies
 let express = require('express')
 let bodyParser = require('body-parser')
+let config = require ('../config.js')
 
 let app = express()
 
 
 // Middleware
-app.use(express.static(__dirname + '/../client'))
+app.use('/asset', express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+// Template
+app.set('view engine', 'ejs')
 
 // Route
 app.get('/', (req, res) => {
 
-	res.send('Salut')
+	res.render('pages/index', {test: 'salut'})
 })
 
-app.listen(4001)
+app.listen(config.port)
